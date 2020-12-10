e45ac3a49deb477790e039255578f9e2d81a2dde
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-11 03:15:44 +0900

    ミドルウェア登録4-2

--- app/Http/Middleware/HelloMiddleware.php
+++ app/Http/Middleware/HelloMiddleware.php
@@ -7,15 +7,17 @@ use Illuminate\Http\Request;
 
 class HelloMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle(Request $request, Closure $next)
-    {
-        return $next($request);
-    }
+   public function handle($request, Closure $next)
+   {
+       $data = [
+           ['name'=>'taro', 'mail'=>'taro@yamada'],
+           ['name'=>'hanako', 'mail'=>'hanako@flower'],
+           ['name'=>'sachiko', 'mail'=>'sachico@happy'],
+       ];
+       $request->merge(['data'=>$data]);
+       return $next($request);
+   }
 }
+
+
+
