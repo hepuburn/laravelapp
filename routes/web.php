65ff719f6184bad5703c6ba428e3e85802df8fe9
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-10 19:35:21 +0900

    クエリー文字使用3-11

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ use Illuminate\Support\Facades\Route;
 // Route::get('hello/{id?}/{pass?}', 'App\Http\Controllers\HelloController@index');
 
 
- Route::get('hello', 'App\Http\Controllers\HelloController@index');
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
