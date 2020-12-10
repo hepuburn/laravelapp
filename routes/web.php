386531b5fd8119efb2ce83f6a5d75b0b529ac7ab
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-10 17:26:05 +0900

    ルート修正2-8

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('hello', 'HelloController@index');
+// パスが足りない
+// Route::get('hello', 'HelloController@index'); 
+Route::get('/hello','App\Http\Controllers\HelloController@index');
