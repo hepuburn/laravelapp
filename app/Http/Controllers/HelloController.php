a5030f557223507b53e908a434d3961ee84a5927
hepuburn <cb400sbw9@yahoo.co.jp>
2020-12-10 23:35:04 +0900

    コレクションビュー3-33

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,10 +10,13 @@ class HelloController extends Controller
   
     public function index()
     {
-       $data = ['one', 'two', 'three', 'four', 'five'];
+       $data = [
+           ['name'=>'山田たろう', 'mail'=>'taro@yamada'],
+           ['name'=>'田中はなこ', 'mail'=>'hanako@flower'],
+           ['name'=>'鈴木さちこ', 'mail'=>'sachico@happy']
+       ];
        return view('hello.index', ['data'=>$data]);
     }
-    
 
 }
 
